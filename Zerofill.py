95d49188cc6323478de1a08a30701a7ad4246024
Mick <23mdrecker@mg.k12.mo.us>
2022-10-13 11:21:28 -0500

    Initial Program Upload

---
+++ Zerofill.py
@@ -0,0 +1,37 @@
+import os
+import sys
+import subprocess
+
+list_blcks = subprocess.Popen(["lsblk"], stdout=subprocess.PIPE)  ## Run lsblk in bash
+list_blcks = list_blcks.stdout.readlines()  ## Read lsblk from bash
+print("Available Blocks: \n %d", list_blcks)  ## Print list_blcks +...
+for line in list_blcks:  ## ...per line lsblk
+    print(line.decode().strip())
+
+print(
+    "Zerofill block or partition\n\nEx: Input: sda1 \n\nBlock or partition input: "
+)  ## Ask input block
+blck = input()  ## Grab input for blck
+print(f"\nIs {blck} correct? [y/n]\n")  ## Print question
+valid = input()  ## Grab input for valid
+if (
+    valid == "n" or valid == "no" or valid == "N" or valid == "NO"
+):  ## Simple filters for typos
+    sys.exit(1)  ## End process with EC 1
+
+zerofill = subprocess.Popen(
+    [
+        "sudo",
+        "dd",
+        "if=/dev/zero",
+        "of=/dev/sd{}",
+        "status=progress",
+        "bs=16M".format(blck),
+    ],
+    stdout=subprocess.PIPE,
+    stdin=subprocess.PIPE,
+)  ## Runs dd in bash with given args
+zerofill.wait()  ## Wait for termination of zerofill
+sync = subprocess.Popen(["sync"], stdout=subprocess.PIPE)  ## Run sync in bash
+
+print("\n------------------------\n         END\n------------------------\n")
